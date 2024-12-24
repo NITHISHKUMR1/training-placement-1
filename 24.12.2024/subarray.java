7eb2e67e2856f9e2e8fe3485c8f2c5feb8a3e950
NITHISHKUMAR.T <151352721+NITHISHKUMR1@users.noreply.github.com>
2024-12-24 17:23:49 +0530

    Create subarray.java

---
+++ 24.12.2024/subarray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class NegativeSubarrayCounter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        scanner.close();
+        int negativeCount = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    negativeCount++;
+                }
+            }
+        }
+
+        System.out.println(negativeCount);
+    }
+}
